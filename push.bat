f927bf704ee60cd2cdf38cc5a50edb9997089eb6
Παπαδόπουλος Αλέξανδρος <it164828@it.teithe.gr>
2022-01-23 22:24:06 +0200

    test2

--- push.bat
+++ push.bat
@@ -5,7 +5,7 @@ set /p v="Commit Title: "
 git commit -m "%v%"
 git push origin main
 set /p b="Do you want to commit to a different branch? (Yes/No)"
-if %b%=Yes (
+if %b%="Yes" (
 	set /p b="Which Branch? : "
 	git push origin main:v1.2
 ) 
